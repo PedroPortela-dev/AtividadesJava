9ad23cc24d59b5f14b7d24b3470659dd5389e09a
manualbqr <101570498+manualbqr@users.noreply.github.com>
2022-03-20 15:35:59 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+   public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("Oii, me chamo Emanuela, mas a maioria das pessoas me chamam d Manu, e tenho 15 anos. Entrei no Sesi nesse ano.");
+    System.out.println("Gosto de muita coisa, mas as principais são livros e músicas. Sobre a música, amo todos os tipo, mas tenho uma paixão por música clássica e meus compositores favoritos são Dvorak, Paganini, Liszt e Chopin.");
+    System.out.println("Vivo com a música desde meus 10 anos, quando entrei para o coral infantil do NEOJIBA. Com meus 11, comecei tocando Violino, e desde então nunca mais parei. Além do violino, toco outros instrumentos também.");
+    System.out.println("Agora sobre os livros, leio desde os 4 anos e sou apaixonada nos clássicos. Quanto mais antigo, melhor KKKKKK. Meus livros favoritos são: o retrato de dorian gray e frankenstein");
+    System.out.println("Não tenho experiencia na área de programação, mas sempre foi uma área que tive muito interesse."); }
+}   
+    
+ 
